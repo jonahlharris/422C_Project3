7c9dafb405679ce65434bd38d4b3d5b6c37ab9be
Hackerman64 <jonahlharris@utexas.edu>
2016-09-27 13:03:39 -0500

    Reorganizing Structure
    I'm trying to get it so we can use the desktop app to automatically update our project.

---
+++ src/assignment3/DFS.java
@@ -0,0 +1,38 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Jonah Harris
+ * jlh6487
+ * 16455
+ * 
+ * Sagar Krishnaraj
+ * 2Kool4Skewl
+ * 1-800-YOUR-MOM
+ * 
+ * Slip days used: <0>
+ * Git URL: https://github.com/Hackerman64/422C_Project3
+ * Fall 2016
+ */
+
+package assignment3;
+import java.util.*;
+import java.io.*;
+
+
+public class DFS {
+	
+	/*
+	 * Returns a word ladder between the 'start' and 'end' words using DFS.
+	 */
+	public ArrayList<String> startDFS(String start, String end, Set<String> the_d){
+		
+		
+		
+		
+		
+		
+		
+		
+		return null; //CHANGE 
+	}
+
+}
---
+++ src/assignment3/WordLadder.java
@@ -0,0 +1,16 @@
+package assignment3;
+import java.util.*;
+import java.io.*;
+
+/*
+ * Class that creates our word ladders from 
+ */
+public class WordLadder {
+	
+	public WordLadder(){
+		
+	}
+	
+	
+
+}
