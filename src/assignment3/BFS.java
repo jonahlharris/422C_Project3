5f5e2b23289432f355a601ee094965a493b6fab6
Sagar <sagar.krishnaraj@gmail.com>
2016-09-30 12:50:32 -0500

    BFS improvements

---
+++ src/assignment3/BFS.java
@@ -0,0 +1,28 @@
+//package assignment3;
+//
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//import java.util.LinkedHashSet;
+//import java.util.Set;
+//
+//public class BFS {
+//	
+//	public static ArrayList<String> getWordLadderBFS(Word start, Word end) {
+//		// initialize data structures
+//		Set<Word> currWords = new LinkedHashSet<Word>();
+//		Iterator<Word> iterator = currWords.iterator();
+////		ArrayList<String> currWordsClone = new ArrayList<String>(currWords.size());
+////    	for (Word item : currWords) currWordsClone.add(item.value);
+//		currWords.add(start);
+//		Set<String> dict = Main.makeDictionary();
+//		
+//		// 
+//		while (!currWordsClone.contains(end.value)) {
+//			// iterate over dictionary
+//			
+//		}
+//		
+//		return null;
+//	}
+//	
+//}
